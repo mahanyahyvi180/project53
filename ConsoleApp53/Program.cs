ab18eaf445d8ce5943fc0cf0c54cb94b25cfa121
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-06 22:55:17 +0330

    Add files via upload

---
+++ ConsoleApp53/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 6;
+
+            ++num;
+            Console.WriteLine(6);
+            Console.ReadLine();
+        }
+    }
+}
